f4c44a6843584ab2e71f2e7b48e4608a6d4ca1cd
Maks Onyshchenko <onyshchenkomaksymm@gmail.com>
2025-05-18 03:09:00 +0300

    create post-details.html/js create function rendering user

--- task_2/user-details.js
+++ task_2/user-details.js
@@ -1,7 +1,25 @@
-
-let userDetails = document.getElementById("userDetails");
+const userDetails = document.getElementById("userDetails");
+const btnPost = document.createElement("button");
+btnPost.innerText = "post of current user";
 const user = JSON.parse(localStorage.getItem("userData"));
+const url = `https://jsonplaceholder.typicode.com/users/${user.id}/posts`
 
-for (const key in user) {
-
+function createUser(obj, container) {
+    const list = document.createElement("ul");
+    for (const key in obj) {
+        const li = document.createElement("li");
+        li.innerText = `${key}: ${typeof obj[key] === "object"? "":obj[key]}`;
+        list.append(li);
+        if(typeof obj[key] === "object"){
+            createUser(obj[key], li);
+        }
+    }
+    container.append(list, btnPost);
 }
+createUser(user, userDetails);
+
+btnPost.addEventListener("click", ()=>{
+    fetch(url).then(res => res.json()).then(posts => {
+        console.log(posts);
+    })
+});
