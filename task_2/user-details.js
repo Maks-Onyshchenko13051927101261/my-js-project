a61244f03da64800933772770cf324988547a0fa
Maks Onyshchenko <onyshchenkomaksymm@gmail.com>
2025-05-18 03:31:47 +0300

    change user-details.html/css

--- task_2/user-details.js
+++ task_2/user-details.js
@@ -1,4 +1,5 @@
 const userDetails = document.getElementById("userDetails");
+const postsTitle = document.getElementById("postsTitle");
 const btnPost = document.createElement("button");
 btnPost.innerText = "post of current user";
 const user = JSON.parse(localStorage.getItem("userData"));
@@ -20,6 +21,21 @@ createUser(user, userDetails);
 
 btnPost.addEventListener("click", ()=>{
     fetch(url).then(res => res.json()).then(posts => {
-        console.log(posts);
+        const postsList = document.createElement("ul");
+        posts.forEach(post => {
+            const postsItem = document.createElement("li");
+            postsItem.innerText = `${post.title}`;
+            const btnLinkPost = document.createElement("button");
+            btnLinkPost.innerText = "post details";
+
+            btnLinkPost.addEventListener("click", () => {
+                localStorage.setItem("postInfo", JSON.stringify(post));
+                window.location.href = `post-details.html`;
+            })
+
+            postsList.append(postsItem, btnLinkPost);
+        })
+        postsTitle.append(postsList)
+
     })
 });
